bd60deab99799402b76f381816767d081d1d3b05
Thrumbar <thrumbar@gmail.com>
2025-09-26 09:10:34 -0500

    Update Toon.lua

--- Modules/Toon.lua
+++ Modules/Toon.lua
@@ -1711,7 +1711,7 @@ function EMA:LFGTeleport( event, arg1, ... )
 end
 
 function EMA:DoLFGTeleport(port)
-	if EMAPrivate.Core.isEmaClassicBccBuild() == ture then return end	
+	if EMAPrivate.Core.isEmaClassicBccBuild() == true then return end	
 	--EMA:Print("TeleCommand", port)
 	EMA.isInternalCommand = true
 	if IsShiftKeyDown() == false then
@@ -1731,10 +1731,10 @@ function EMA:LFG_ROLE_CHECK_SHOW( event, ... )
 			local characterName = EMAUtilities:AddRealmToNameIfMissing( leadername )
 			--EMA:Print("test", characterName)
 			if EMAApi.IsCharacterInTeam( characterName) == true then
-				CompleteLFGRoleCheck("ture")
+				CompleteLFGRoleCheck("true")
 			end
 		else
-			CompleteLFGRoleCheck("ture")
+			CompleteLFGRoleCheck("true")
 		end
 	end	
 end
